db822546bf626dc5aeb9b7b7415d114932da6dae
Denis Kruchok <denis.kruchok.cv@gmail.com>
2024-06-14 22:12:54 +0300

    Git info log was aligned

--- Sources/Patterns.cpp
+++ Sources/Patterns.cpp
@@ -2,20 +2,24 @@
 
 #include "Patterns.h"
 
+#include <iomanip>
 #include <iostream>
 #include "ChainOfResponsibility.h"
 #include "Command.h"
 
 int main()
 {
-	std::cout << "\nProject Version: " << PATTERNS_VERSION_MAJOR << "."
-									 << PATTERNS_VERSION_MINOR << "."
-									 << PATTERNS_VERSION_PATCH << std::endl;
+	std::cout << "\n";
+	std::cout << std::left << std::setw(30) << "Project Version: "
+											<< PATTERNS_VERSION_MAJOR << "."
+											<< PATTERNS_VERSION_MINOR << "."
+											<< PATTERNS_VERSION_PATCH << std::endl;
 
-	std::cout << "Current changelist hash : " << GIT_COMMIT_HASH << std::endl;
-	std::cout << "Commit message : " << GIT_COMMIT_MESSAGE << std::endl;
-	std::cout << "Author : " << GIT_COMMIT_AUTHOR << std::endl;
-	std::cout << "Time : " << GIT_COMMIT_TIME << std::endl;
+	std::cout << std::left << std::setw(30) << "Current changelist hash : "
+											<< GIT_COMMIT_HASH << std::endl;
+	std::cout << std::left << std::setw(30) << "Commit message : " << GIT_COMMIT_MESSAGE << std::endl;
+	std::cout << std::left << std::setw(30) << "Author : " << GIT_COMMIT_AUTHOR << std::endl;
+	std::cout << std::left << std::setw(30) << "Time : " << GIT_COMMIT_TIME << std::endl;
 
 	std::cout << "\n=== ChainOfResponsibility Pattern ===\n";
 	ChainOfResponsibility::TestChainOfResponsibilityPattern();
